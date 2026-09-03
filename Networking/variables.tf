016ab0bfc8cfe522217c9f44ee8a223f07841bec
ikigai-crypto <eodekunle52@gmail.com>
2026-09-03 17:11:57 +0100

    Define networking variables in variables.tf
    Add variables for environment, VPC CIDR, and availability zones.

--- Networking/variables.tf
+++ Networking/variables.tf
@@ -1,1 +1,18 @@
+#Tradecore networking variables
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "production"
+}
 
+variable "vpc_cidr" {
+  description = "Tradecore VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["af-south-1a", "af-south-1b"]
+}
