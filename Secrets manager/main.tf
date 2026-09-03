8f22c1d386a5f95ae260f3853c547ae41ea141b3
ikigai-crypto <eodekunle52@gmail.com>
2026-09-03 17:53:05 +0100

    Update main.tf

--- Secrets manager/main.tf
+++ Secrets manager/main.tf
@@ -1,1 +1,56 @@
+# TradeCore Secrets Manager
 
+# DB_HOST
+
+resource "aws_secretsmanager_secret" "db_host" {
+  name       = "/tradecore/${var.environment}/db-host"
+
+  tags = {
+    Name        = "tradecore-${var.environment}-db-host"
+    Environment = var.environment
+  }
+}
+
+# DB_NAME
+
+resource "aws_secretsmanager_secret" "db_name" {
+  name       = "/tradecore/${var.environment}/db-name"
+
+  tags = {
+    Name        = "tradecore-${var.environment}-db-name"
+    Environment = var.environment
+  }
+}
+
+# DB_USER
+
+resource "aws_secretsmanager_secret" "db_user" {
+  name       = "/tradecore/${var.environment}/db-user"
+
+  tags = {
+    Name        = "tradecore-${var.environment}-db-user"
+    Environment = var.environment
+  }
+}
+
+# DB_PASSWORD
+
+resource "aws_secretsmanager_secret" "db_password" {
+  name       = "/tradecore/${var.environment}/db-password"
+
+  tags = {
+    Name        = "tradecore-${var.environment}-db-password"
+    Environment = var.environment
+  }
+}
+
+# JWT_SECRET
+
+resource "aws_secretsmanager_secret" "jwt_secret" {
+  name       = "/tradecore/${var.environment}/jwt-secret"
+
+  tags = {
+    Name        = "tradecore-${var.environment}-jwt-secret"
+    Environment = var.environment
+  }
+}
