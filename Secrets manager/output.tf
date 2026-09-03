f3a639bdb258672a969e2a96596707b3b7ad4600
ikigai-crypto <eodekunle52@gmail.com>
2026-09-03 17:54:36 +0100

    Add outputs for database and JWT secret ARNs
    Added outputs for various secrets including DB_HOST, DB_NAME, DB_USER, DB_PASSWORD, and JWT_SECRET.

--- Secrets manager/output.tf
+++ Secrets manager/output.tf
@@ -1,1 +1,37 @@
+output "db_host_secret_arn" {
+  description = "ARN of the DB_HOST secret"
+  value       = aws_secretsmanager_secret.db_host.arn
+}
+
+output "db_name_secret_arn" {
+  description = "ARN of the DB_NAME secret"
+  value       = aws_secretsmanager_secret.db_name.arn
+}
+
+output "db_user_secret_arn" {
+  description = "ARN of the DB_USER secret"
+  value       = aws_secretsmanager_secret.db_user.arn
+}
+
+output "db_password_secret_arn" {
+  description = "ARN of the DB_PASSWORD secret"
+  value       = aws_secretsmanager_secret.db_password.arn
+}
+
+output "jwt_secret_arn" {
+  description = "ARN of the JWT_SECRET secret"
+  value       = aws_secretsmanager_secret.jwt_secret.arn
+}
+
+output "secret_arns" {
+  description = "ARNs of all TradeCore application secrets"
+
+  value = {
+    db_host     = aws_secretsmanager_secret.db_host.arn
+    db_name     = aws_secretsmanager_secret.db_name.arn
+    db_user     = aws_secretsmanager_secret.db_user.arn
+    db_password = aws_secretsmanager_secret.db_password.arn
+    jwt_secret  = aws_secretsmanager_secret.jwt_secret.arn
+  }
+}
 
