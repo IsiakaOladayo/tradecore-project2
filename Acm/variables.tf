0aba68ea055cdf709cf70a76e9d9f15f2bcaf9fa
enofei <usunobunfavour0@gmail.com>
2026-09-06 14:25:35 -0700

    Clean up comments in variables.tf
    Removed header comments from variables.tf

--- Acm/variables.tf
+++ Acm/variables.tf
@@ -1,6 +1,4 @@
-# =========================================================
 # TRADECORE - ACM VARIABLES
-# =========================================================
 
 variable "project_name" {
   description = "Name of the application/project."
