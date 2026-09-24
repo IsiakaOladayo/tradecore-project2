efb1567bd31208b7dfb4b8c2d194b12db6beb26e
enofei <usunobunfavour0@gmail.com>
2026-09-24 14:26:14 -0700

    refactor: frontend canonical on owner Amplify app (dpqtxdawh7h1c)

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -94,7 +94,7 @@ variable "node_env" {
 variable "frontend_url" {
   description = "Allowed CORS origins for the frontend (comma-separated)."
   type        = string
-  default     = "https://main.d4309xy66jfub.amplifyapp.com"
+  default     = "https://main.dpqtxdawh7h1c.amplifyapp.com"
 }
 
 variable "db_ssl" {
@@ -183,13 +183,13 @@ variable "cognito_logout_urls" {
 variable "amplify_app_id" {
   description = "ID of the console-managed Amplify app (frontend is deployed outside Terraform)."
   type        = string
-  default     = "d4309xy66jfub"
+  default     = "dpqtxdawh7h1c"
 }
 
 variable "amplify_domain" {
   description = "Default domain of the console-managed Amplify app."
   type        = string
-  default     = "main.d4309xy66jfub.amplifyapp.com"
+  default     = "main.dpqtxdawh7h1c.amplifyapp.com"
 }
 
 variable "github_org" {
