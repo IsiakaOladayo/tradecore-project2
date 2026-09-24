cfab5918911128c7bf7421f71f030b38adad6f4f
enofei <usunobunfavour0@gmail.com>
2026-09-24 12:18:09 -0700

    feat: new API image (JWT hardening) + CORS for new Amplify domain

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -94,7 +94,7 @@ variable "node_env" {
 variable "frontend_url" {
   description = "Allowed CORS origin for the frontend."
   type        = string
-  default     = "https://d2rvcx1xtsfbat.amplifyapp.com"
+  default     = "https://develop.dpqtxdawh7h1c.amplifyapp.com"
 }
 
 variable "db_ssl" {
